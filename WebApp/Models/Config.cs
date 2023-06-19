5530a0928d5b712d6979487e022cf5c57152fb22
BuildTools <unconfigured@null.spigotmc.org>
2023-06-19 19:47:51 +0200

    Basic config builder

---
+++ WebApp/Models/Config.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Security;
+
+namespace WebApp.Models;
+
+public class Config
+{
+    [Required]
+    [Url]
+    public string? url { get; set; }
+    [DefaultValue(false)]
+    public bool loop { get; set; }
+    public string? driver { get; set; }
+    [Required]
+    [Range(0, 100000)]
+    public int waitTime { get; set; }
+    //public List<CommandConfig>? Commands { get; set; }
+    
+}
