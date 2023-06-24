9e78b04b614d5b25ed9183977764a34bb61fc8c8
TheKeron <xkeron19@gmail.com>
2023-06-24 23:43:29 +0200

    Generate commands in config builder

--- WebApp/Models/Config.cs
+++ WebApp/Models/Config.cs
@@ -1,20 +1,23 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Security;
-
 namespace WebApp.Models;
 
 public class Config
 {
     [Required]
     [Url]
+    [Display(Name = "URL")]
+    [DataType(DataType.Text)]
     public string? url { get; set; }
     [DefaultValue(false)]
+    [Display(Name = "Loop")]
     public bool loop { get; set; }
+    [Display(Name = "Select a driver")]
+    [DataType("DropdownList")]
     public string? driver { get; set; }
     [Required]
-    [Range(0, 100000)]
+    [Range(0, 10000000000000)]
+    [Display(Name = "WaitTime")]
     public int waitTime { get; set; }
-    //public List<CommandConfig>? Commands { get; set; }
-    
+    public List<CommandConfig>? commands { get; set; }
 }
---
+++ WebScraper/CommandServices.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+
+namespace WebScraper;
+
+public static class CommandServices
+{
+    public static List<string> GetCommandsNames()
+    {
+        var assembly = Assembly.GetExecutingAssembly().GetTypes();
+        var commands = assembly.Where(name =>
+                name.FullName!.Contains($@"WebScraper.SeleniumCommands"))
+            .ToArray();
+        return (from command in commands where command.IsPublic select command.Name).ToList();
+    }
+
+    public static PropertyInfo[] GetProperties(string? commandName)
+    {
+        var assembly = Assembly.GetExecutingAssembly().GetTypes();
+        var command = assembly.Where(name =>
+            name.FullName!.Contains($@"WebScraper.SeleniumCommands.{commandName}")).ToArray()[0];
+        
+        return command.GetProperties();
+    }
+}
